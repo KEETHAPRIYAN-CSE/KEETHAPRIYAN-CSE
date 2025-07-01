3b7ad9b60be4d202969217f6fea63945a27e1e24
KEETHAPRIYAN-CSE <mrkeethapriyan2007@gmail.com>
2025-07-01 19:20:11 +0530

    ARITHMETIC TERM AND IT'S LOGIC

---
+++ arithmetic.java
@@ -0,0 +1,16 @@
+package PORTSIDE;
+
+import java.util.Scanner;
+public class arithmetic {
+    public static void main(String[] args) {
+        Scanner vari = new Scanner(System.in);
+        System.out.println("Enter first term: ");
+        int a = vari.nextInt();
+        System.out.println("Enter common difference: ");
+        int d = vari.nextInt();
+        System.out.println("Enter number of terms: ");  
+        int n = vari.nextInt();
+        int nthTerm = a + (n - 1) * d;
+        System.out.println("The nth term of the arithmetic progression is: " + nthTerm);
+    }
+}
